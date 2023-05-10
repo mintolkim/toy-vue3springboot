485a390822f5d369f317948f1d181c90273c5894
mintol <juna216@naver.com>
2023-05-10 23:15:22 +0900

    vue 연동 확인 커밋

--- src/main/java/com/example/toyproject/Controller/COController.java
+++ src/main/java/com/example/toyproject/Controller/COController.java
@@ -3,7 +3,6 @@ package com.example.toyproject.Controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class COController {
     @GetMapping("test")
-    public void getTest(){
-        System.out.println("test Controller");
-        log.info("테스트 컨트롤러");
+    public String getTest(){
+        return "vue 연동 끝";
     }
 }
