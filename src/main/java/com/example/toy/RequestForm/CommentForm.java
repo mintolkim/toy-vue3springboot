08c377dc2de9ee944ebe6e799a2ff4a16e6dfeb8
mintol <juna216@naver.com>
2023-06-09 16:06:55 +0900

    commentForm 작성

--- src/main/java/com/example/toy/RequestForm/CommentForm.java
+++ src/main/java/com/example/toy/RequestForm/CommentForm.java
@@ -1,9 +1,27 @@
 package com.example.toy.RequestForm;
 
+import com.example.toy.Entity.Date;
+import com.example.toy.Entity.Post;
+import com.example.toy.Entity.User;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class CommentForm {
+
+    private Long id;
+
+    private LocalDateTime updateDate;
+
+    private LocalDateTime writeDate;
+
+    private Long post_id;
+
+    private Long user_id;
+
+    private String content;
 }
