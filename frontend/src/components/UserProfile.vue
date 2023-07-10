f7783a21b5365f15f548da1a226f597a64a2f4f2
mintol <juna216@naver.com>
2023-07-11 07:21:13 +0900

    동적 메뉴 수정

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -22,7 +22,7 @@
                 </h4>
               <h4 v-else class="text-center fst-italic" style="display: flex; justify-content: center; align-items: center;">
                 <input class="fst-italic" v-model="nickName" style="border: none; width: 8rem; margin-right: 5px">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square-fill" viewBox="0 0 16 16" @click="updateNickname">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square-fill" viewBox="0 0 16 16" @click="updateBlogInfo">
                   <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/>
                 </svg>
               </h4>
@@ -76,6 +76,7 @@ export default {
       const oldNick = ref(null);
       const menu = ref(null);
       const menuCnt = ref(0);
+      const newMenuCnt = ref(0);
 
       onMounted(async () => {
         try {
@@ -101,6 +102,7 @@ export default {
         const response = await api.post('/api/selectMenu', { username : username });
         menu.value = response.data.result;
         menuCnt.value = response.data.result.length;
+        newMenuCnt.value = response.data.result.length;
       }
 
       // 업데이트 알림
@@ -124,28 +126,42 @@ export default {
         console.log("파일 업로드");
       }
 
-      // 닉네임 변경
-      const updateNickname = async () => {
+      //
+      const updateBlogInfo = async () => {
           updateBlog.value = true;
-          if(oldNick.value !== nickName.value){ // 값이 변경되었는지 확인
-            // 유저 정보
-            const responseUser = await api.post('api/userInfo/'+blogRoute);
-            responseUser.userId = responseUser.data.result;
-            const response = await api.post('api/updateNick', {id : responseUser.userId, nickname: nickName.value});
-            console.log(response)
+          console.log(newMenuCnt.value)
+          console.log(menuCnt.value)
+          if(oldNick.value !== nickName.value || newMenuCnt.value !== menuCnt.value){ // 값이 변경되었는지 확인
+            //유저정보 가져오기
+            const responseUser = await userInfo();
+            // 닉네임 변경
+            const result = await updateNickname(responseUser);
+            console.log(result);
             setTimeAlert(true);
-            setMessage(response.data.message);
+            setMessage("수정사항이 변경 되었습니다.");
             updateBlog.value = false;
             await infoBlog();
           }
       }
 
+      // 닉네임 변경
+      const updateNickname = async (user) => {
+        return await api.post('api/updateNick', {id : user, nickname: nickName.value});
+      }
+
+      // 유저 정보 불러오기
+      const userInfo = async () => {
+        const responseUser = await api.post('api/userInfo/'+blogRoute);
+        return responseUser.userId = responseUser.data.result;
+      }
+
+
       const deleteMenu = (menuObject) => {
-        if (menuCnt.value <= 1) {
+        if (newMenuCnt.value <= 1) {
           setTimeAlert(true);
           setMessage("메뉴 하나 이상은 필수 입니다.");
         } else {
-          menuCnt.value--;
+          newMenuCnt.value--;
           menuObject.hidden = true;
         }
       }
@@ -167,7 +183,7 @@ export default {
       const updateStatus = computed(() => store.state?.user?.username);
       return {
         updateStatus, blogRoute, profileImg, nickName, setBlog, updateBlog,
-        uploadProfile, updateNickname, setTimeAlert, setMessage, menu, menuCnt, updateAlert, menuLoad,
+        uploadProfile, updateBlogInfo, setTimeAlert, setMessage, menu, menuCnt, updateAlert, menuLoad,
         updateMenu, plusMenu, deleteMenu
       }
   }
