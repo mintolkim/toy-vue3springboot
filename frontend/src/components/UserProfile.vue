0a0e9cf5a5c9074951194bdc6475f9ce693b8db9
mintol <juna216@naver.com>
2023-07-11 07:03:31 +0900

    메뉴 수정 중

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -13,7 +13,7 @@
               </div>
             </div>
             <div class="col">
-                <h4 class="fst-italic" v-if="!updateBlog"> &nbsp;
+                <h4 class="fst-italic" v-if="!updateAlert"> &nbsp;
                   {{ nickName }}
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16" v-if="blogRoute == updateStatus && !updateBlog" @click="setBlog">
                     <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
@@ -27,8 +27,22 @@
                 </svg>
               </h4>
             </div>
+          <div v-for="menuObject in menu" :key="menuObject.id">
+            <div v-if="updateAlert && !menuObject.hidden">
+              <input @change="updateMenu(menuObject)" v-model="menuObject.menuName" style="border: none; width: 8rem; margin-right: 5px">
+              <svg @click="deleteMenu(menuObject, menu)" style="color: orangered" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z"/>
+              </svg>
+            </div>
+            <a href="javascript:void(0)" v-else-if="!menuObject.hidden" >
+              {{menuObject.menuName}}
+            </a>
+          </div>
+          <svg v-if="updateAlert" @click="plusMenu" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+          </svg>
         </div>
-        <greed-menu :updateBlog="updateBlog"/>
     </div>
     <div class="card">
         <todayWeather/>
@@ -36,7 +50,6 @@
 </template>
 
 <script>
-import greedMenu from "@/components/GreedMenu.vue"
 import todayWeather from "@/components/TodayWeather.vue"
 import {useStore} from "vuex";
 import {computed, onMounted, ref} from "vue";
@@ -51,7 +64,6 @@ export default {
         }
     },
     components : {
-        greedMenu,
         todayWeather
     },
   setup(){
@@ -62,11 +74,16 @@ export default {
       const nickName = ref('');
       const updateBlog = ref(false);
       const oldNick = ref(null);
+      const menu = ref(null);
+      const menuCnt = ref(0);
+
       onMounted(async () => {
         try {
           await infoBlog();
+          await menuLoad();
         } catch (e) {
-          console.log(e);
+          setTimeAlert(true);
+          setMessage("에러가 발생하였습니다.");
         }
       });
 
@@ -75,9 +92,25 @@ export default {
 
       // 블로그 셋팅
       const setBlog = () => {
-        console.log("블로그")
         updateBlog.value = true;
       }
+
+      // 메뉴 읽어오기
+      const menuLoad = async () => {
+        const username = route.params.id;
+        const response = await api.post('/api/selectMenu', { username : username });
+        menu.value = response.data.result;
+        menuCnt.value = response.data.result.length;
+      }
+
+      // 업데이트 알림
+      const updateAlert = computed(() => {
+        if (updateBlog.value){
+          setTimeAlert(true);
+          setMessage("v 버튼을 누르면 변경사항이 저장됩니다.");
+        }
+        return updateBlog.value
+      })
 
       // 블로그 정보 불러오기
       const infoBlog = async () => {
@@ -107,9 +140,35 @@ export default {
           }
       }
 
+      const deleteMenu = (menuObject) => {
+        if (menuCnt.value <= 1) {
+          setTimeAlert(true);
+          setMessage("메뉴 하나 이상은 필수 입니다.");
+        } else {
+          menuCnt.value--;
+          menuObject.hidden = true;
+        }
+      }
+
+      const updateMenu = (menuObject) => {
+        console.log(menuObject.id);
+        console.log(menuObject.menuName)
+      }
+
+      const plusMenu = () => {
+        menu.value.push({
+          id: Date.now(), // 가짜 ID, 실제 구현에서는 유니크한 ID를 제공해야 함
+          menuName: 'menu',   // 초기 메뉴 이름
+          hidden: false,  // 초기 hidden 값
+        });
+        menuCnt.value++;
+      }
+
       const updateStatus = computed(() => store.state?.user?.username);
       return {
-        updateStatus, blogRoute, profileImg, nickName, setBlog, updateBlog, uploadProfile, updateNickname, setTimeAlert, setMessage
+        updateStatus, blogRoute, profileImg, nickName, setBlog, updateBlog,
+        uploadProfile, updateNickname, setTimeAlert, setMessage, menu, menuCnt, updateAlert, menuLoad,
+        updateMenu, plusMenu, deleteMenu
       }
   }
 }
